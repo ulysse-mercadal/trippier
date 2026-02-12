e9a50fcc331c675d3a43536ceb816fa15da20b68
Ulysse Mercadal <ulysse.mercadal@epitech.eu>
2026-02-12 22:23:58 +0100

    feat: front redirect on loging errro fixed

--- frontend/lib/client.ts
+++ frontend/lib/client.ts
@@ -34,7 +34,7 @@ client.interceptors.request.use(
 client.interceptors.response.use(
   response => response,
   error => {
-    if (error.response?.status === 401) {
+    if (error.response?.status === 401 && !error.config.url?.includes('/auth/login')) {
       if (typeof window !== 'undefined') {
         localStorage.removeItem('@Trippier:token');
         localStorage.removeItem('@Trippier:user');
